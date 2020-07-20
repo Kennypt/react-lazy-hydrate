6650b98135e7c92a4d90781c0ae39ac276fa5cf8
Ricardo Pinheiro <ricardo.pinheiro@thetrainline.com>
2020-07-20 02:30:31 +0100

    udpate

--- webpack.config.js
+++ webpack.config.js
@@ -3,19 +3,35 @@ const webpack = require("webpack");
 
 module.exports = {
   entry: {
-    'react-lazy-hydrate': path.resolve(__dirname, "./src/index.jsx"),
+    'react-lazy-hydrate': path.resolve(__dirname, './src/index.jsx'),
   },
   output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "[name].js",
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].js',
   },
   module: {
-    rules: [{ test: /\.jsx$/, exclude: /node_modules/, loader: "babel-loader" }],
+    rules: [
+      { test: /\.jsx$/, exclude: /node_modules/, loader: 'babel-loader' },
+    ],
+  },
+  devtool: 'source-map',
+  externals: {
+    react: {
+      commonjs: 'react',
+      commonjs2: 'react',
+      amd: 'react',
+      // React dep should be available as window.React, not window.react
+      root: 'React',
+    },
+    'react-dom': {
+      commonjs: 'react-dom',
+      commonjs2: 'react-dom',
+      amd: 'react-dom',
+      root: 'ReactDOM',
+    },
   },
   optimization: {
-    runtimeChunk: "single",
+    minimize: false,
   },
-  plugins: [
-    new webpack.ProgressPlugin({ percentBy: "entries" }),
-  ],
+  plugins: [new webpack.ProgressPlugin({ percentBy: 'entries' })],
 };
